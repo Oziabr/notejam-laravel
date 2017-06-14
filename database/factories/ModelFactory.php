ffb5cd0bde32cea2bd8fadfe8beba90632df6c6f
Yury Tvardosky <oziabr@gmail.com>
2017-06-14 14:34:12 +0300

    Data seed; close #10

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,22 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->userName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Pad::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+    ];
+});
+
+$factory->define(App\Note::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->catchPhrase,
+        'text' => $faker->realtext,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+        factory(App\User::class, 3)->create();
+
+        App\User::all()->each(function ($u) {
+            $u->pads()->save(factory(App\Pad::class)->make());
+            $u->pads()->save(factory(App\Pad::class)->make());
+            $u->pads()->save(factory(App\Pad::class)->make());
+            $u->pads()->save(factory(App\Pad::class)->make());
+            $u->pads()->save(factory(App\Pad::class)->make());
+        });
+
+        App\Pad::all()->each(function ($p) {
+            $p->notes()->save(factory(App\Note::class)->make());
+            $p->notes()->save(factory(App\Note::class)->make());
+            $p->notes()->save(factory(App\Note::class)->make());
+            $p->notes()->save(factory(App\Note::class)->make());
+            $p->notes()->save(factory(App\Note::class)->make());
+        });
+
     }
 }
